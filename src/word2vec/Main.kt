a39c30b881512cd9a7914dd4fc3cbf995555e0fe
Mitsuhiro0911 <outoforder3216@gmail.com>
2019-07-29 21:06:30 +0900

    ①Wikipediaの要旨のみを使った軽量化コーパスを用いたモードも扱えるようにした ②コサイン類似度が高い単語トップ１０を出力する処理を追加 ③入力した単語(演算で用いる単語)はコサイン類似度の計算をしないように条件を追加

--- src/word2vec/Main.kt
+++ src/word2vec/Main.kt
@@ -5,21 +5,20 @@ import java.io.BufferedReader
 import java.io.File
 import java.io.FileReader
 
-//TODO:コーパスが大きすぎるので、名詞のみに限定するといった方法で削減する
-//TODO:マップに大規模コーパスの情報すべてを格納は難しい。入力した単語だけマップにputし、数回コーパスを走査する仕様にする
 
 fun main(args: Array<String>) {
     val cal = Calculator()
 
     // 入力した単語の素性ベクトル情報をサーチ
-    val searchBr = BufferedReader(FileReader(File("data/corpas/model_201907.vec")))
+//    val searchBr = BufferedReader(FileReader(File("data/corpas/model_201907.vec")))
+    val searchBr = BufferedReader(FileReader(File("data/corpas/model_abstract_201907.vec")))
     var searchStr = searchBr.readLine()
     searchStr = searchBr.readLine()
     val vectorMap = LinkedHashMap<String, ArrayList<Double>>()
     while(searchStr != null){
-        println(searchStr)
+//        println(searchStr)
         val split = searchStr.split(" ")
-        if(split[0] == "パズドラ" || split[0] == "ゲーム") {
+        if(split[0] == "アンドロイド" || split[0] == "感情") {
             val scoreList = ArrayList<Double>()
             for (i in 1 until split.size - 1) {
                 scoreList.add(split.get(i).toDouble())
@@ -35,33 +34,49 @@ fun main(args: Array<String>) {
     val calculatedVector = LinkedHashMap<String, ArrayList<Double>>()
     var joinedWord = ""
     val calculatedScore = ArrayList<Double>()
-    for(i in 0 until 300){
+    for(i in 0 until 200){
         calculatedScore.add(0.0)
     }
     for(vector in vectorMap) {
         joinedWord = joinedWord + vector.key
         for(i in 0 until vector.value.size) {
-            calculatedScore[i] = calculatedScore[i] +  vector.value.get(i)
+            calculatedScore[i] = calculatedScore[i] + vector.value.get(i)
         }
     }
     calculatedVector.put(joinedWord, calculatedScore)
     println(calculatedVector)
 
     // 入力した単語と他の単語のコサイン類似度を計算
-    val cosBr = BufferedReader(FileReader(File("data/corpas/model_201907.vec")))
+//    val cosBr = BufferedReader(FileReader(File("data/corpas/model_201907.vec")))
+    val cosBr = BufferedReader(FileReader(File("data/corpas/model_abstract_201907.vec")))
+    var cosRank = LinkedHashMap<String, Double>()
     var cosStr = cosBr.readLine()
     cosStr = cosBr.readLine()
     while(cosStr != null){
         val scoreList = ArrayList<Double>()
         val split = cosStr.split(" ")
-        for (i in 1 until split.size - 1) {
-            scoreList.add(split.get(i).toDouble())
+        // 演算に用いた単語は除外
+        if (split[0] != "アンドロイド" && split[0] != "感情") {
+            for (i in 1 until split.size - 1) {
+                scoreList.add(split.get(i).toDouble())
+            }
+            val word = calculatedVector.get(joinedWord)!!.toDoubleArray()
+            val compareWord = scoreList.toDoubleArray()
+            val cosSimilarity = cal.calCosSimilarity(word, compareWord)
+//            println(cosSimilarity)
+            cosRank.put(split[0], cosSimilarity)
         }
-        val word = calculatedVector.get(joinedWord)!!.toDoubleArray()
-        val compareWord = scoreList.toDoubleArray()
-        val cosSimilarity = cal.calCosSimilarity(word, compareWord)
-        println(cosSimilarity)
+//        cosRank = cosRank.toList().sortedByDescending { it.second }.toMap()
         cosStr = cosBr.readLine()
     }
     cosBr.close()
+    val sortedAnswer = cosRank.toList().sortedByDescending { it.second }.toMap()
+    var i = 0
+    for(ans in sortedAnswer){
+        i = i.plus(1)
+        println("${i}位：${ans}")
+        if(i > 9) {
+            break
+        }
+    }
 }
